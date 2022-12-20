352b2e5a7ce9ad49015596d624140dac8c171146
Kneesal <114973713+Kneesal@users.noreply.github.com>
2022-12-20 17:49:25 +1300

    feat: added logo with react-tilt effect

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import "./Logo.css"
+import brain from './artificial-intelligence.png'
+
+const Logo = () => {
+    return(
+            <div className='ma4 mt0 '>
+            <Tilt  className='br2 shadow-2 h4 w4 tilt pa2' perspective={700} gyroscope={true}>
+                    <img src = {brain} alt = 'logo'></img>
+             </Tilt>
+            </div>
+    )
+}
+
+export default Logo
