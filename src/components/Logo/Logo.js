806ada94e6f417f0d29290b972934ff0c87fbbd0
Kneesal <114973713+Kneesal@users.noreply.github.com>
2022-12-21 23:41:52 +1300

    feat: added image link input box and button css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import brain from './artificial-intelligence.png'
 const Logo = () => {
     return(
             <div className='ma4 mt0 '>
-            <Tilt  className='br2 shadow-2 h4 w4 tilt pa2' perspective={700} gyroscope={true}>
+            <Tilt  className='br2 shadow-2 h4 w4 tilt pa2' perspective={500} gyroscope={true}>
                     <img src = {brain} alt = 'logo'></img>
              </Tilt>
             </div>
