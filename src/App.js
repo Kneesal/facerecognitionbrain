f65d1f7b30617ec56ccf14e600f152beddbb03cc
Kneesal <114973713+Kneesal@users.noreply.github.com>
2022-12-23 00:21:31 +1300

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import './App.css'
 
+
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <Navigation/>
-        <Logo/>
-        <Rank/>
-        <ImageLinkForm/>
-        {/* <FaceRecognition/> */}
+          <ParticlesBg type="cobweb" num={200} bg={(true)} color = "#222426"/>
+          <Navigation/>
+          <Logo/>
+          <Rank/>
+          <ImageLinkForm/>
+          {/* <FaceRecognition/> */}
       </div>
     );
   }
