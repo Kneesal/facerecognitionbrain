27173a08d7701178f53aea8bc87728ec6ced772c
Kneesal <114973713+Kneesal@users.noreply.github.com>
2022-12-26 10:06:42 +1300

    chore: got bounding box data and created calculations function

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,14 @@
-import React from "react"
+import React from "react";
 
-const FaceRecognition = ({imageUrl}) => {
-   return imageUrl ? (
-        <div className="center ma">
-            <div className="absolute mt2">
-                <img src={imageUrl} alt="stock" width='500px' height = 'auto'></img>
-            </div>
-        </div>
-    ) : null
-}
+const FaceRecognition = ({ imageUrl, box }) => {
+  return imageUrl ? (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img id='inputimage' src={imageUrl} alt="stock" width="500px" height="auto"></img>
+        <div className="bounding-box"></div>
+      </div>
+    </div>
+  ) : null;
+};
 
-export default FaceRecognition
+export default FaceRecognition;
